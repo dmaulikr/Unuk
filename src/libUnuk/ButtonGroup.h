7f0ba86f4840a0c905d8c28fe30bec10ab0ddda7
Tamir Atias <engine.games@gmail.com>
2012-01-10 06:35:23 +0200

    [Fix] Forgot to add ButtonGroup.h and ButtonGroup.cpp.

---
+++ src/libUnuk/ButtonGroup.h
@@ -0,0 +1,35 @@
+#ifndef _BUTTONGROUP_H_
+#define _BUTTONGROUP_H_
+#include <list>
+
+#include "Button.h"
+
+class ButtonGroup {
+public:
+  ButtonGroup(void);
+  ~ButtonGroup(void);
+
+  void  CheckMouseOverDummy(void);
+  int   CheckMouseOver(void);
+
+  void RenderLiteral(void);
+
+  void AddButton(Button* button);
+  void RemoveButton(int index);
+
+  void SelectNext(void);
+  void SelectPrevious(void);
+
+  int   GetSelectedButton(void)         { return _selectedButton; }
+  void  SetSelectedButton(int button);
+
+  Button* GetButton(int index);
+
+private:
+  void HighlightNewSelection(int newButton);
+
+  std::list<Button*> _buttons;
+  int _selectedButton;
+};
+
+#endif
