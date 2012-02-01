15cbf5ce556abe161c20b55575515aa7ffa828e5
Allanis <allanis@saracraft.net>
2012-02-01 23:44:04 +0000

    [Add] Getting to work on pathfinding, my last one was not complete. [Fix] Fixed .gitignore

--- src/libUnuk/Engine/NPC.h
+++ src/libUnuk/Engine/NPC.h
@@ -1,26 +1,25 @@
 #pragma once
 
 #include "Character.h"
-#include "AStar.h"
 
 class NPC : public Character {
 public:
-  NPC(LevelGen* mapArg);
-  ~NPC(void);
-  
-  void Update(void);
+	NPC(LevelGen* mapArg);
+	~NPC(void);
+
+	void Update(void);
 
 protected:
-  void Move(void);
-  
+	void Move(void);
+
 private:
-  int _moveChangeFrequency;
-  
-  int _moveDurationCurrent;
-  int _moveDurationMin;
-  int _moveDurationMax;
-  
-  bool _moving;
-  
-  Timer _moveTimer;
+	int _moveChangeFrequency;
+
+	int _moveDurationCurrent;
+	int _moveDurationMin;
+	int _moveDurationMax;
+
+	bool _moving;
+
+	Timer _moveTimer;
 };
---
+++ src/libUnuk/Engine/Pathfinding.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+#include <assert.h>
+#include <algorithm>
+#include <set>
+#include <vector>
+using namespace std;
+
+// Disable warning that debugging information has lines that are truncated.
+#ifdef WIN32
+#pragma warning(disable : 4786)
+#endif
+
+// The search class. UserState is the users state space type.
+template<class UserState> class AStarSearch {
+public:
+	enum {
+		SEARCH_STATE_NOT_INITIALISED,
+		SEARCH_STATE_SEARCHING,
+		SEARCH_STATE_SUCCEEDED,
+		SEARCH_STATE_FAILED,
+		SEARCH_STATE_OUT_OF_MEMORY,
+		SEARCH_STATE_INVALID
+	};
+
+	// A node representing a possible state in the search.
+	class Node {
+	public:
+		// Keep a record of successor nodes.
+		Node* parent;
+		// Used to view the search in reverse at the end.
+		Node* child;
+
+		float g; // Cost of this and it's predecessors.
+		float h; // Heuristic estimate of the distance of the goal.
+		float f; // Sum of cost and heuristic.
+
+		Node(void) : parent(0), child(0), g(0.0f), h(0.0f), f(0.0) {}
+
+		UserState _UserState;
+	};
+};
