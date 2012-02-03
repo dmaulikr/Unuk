733c541d1180d25c9e5db46838b6671bc3ef4d15
Allanis <allanis@saracraft.net>
2012-02-03 15:09:28 +0000

    [Add] Working some more on AStar pathfinding. This is going to get messy.

--- src/libUnuk/Engine/Pathfinding.cpp
+++ src/libUnuk/Engine/Pathfinding.cpp
@@ -1,1 +1,23 @@
 #include "Pathfinding.h"
+
+template<typename UserState>
+AStarSearch<UserState>::AStarSearch(void) :
+		_state(SEARCH_STATE_NOT_INITIALISED),
+		_currentSolutionNode(NULL),
+		_allocateNodeCount(0),
+		_cancelRequest(false) {}
+
+template<typename UserState>
+AStarSearch<UserState>::~AStarSearch(void) {
+
+}
+
+template<typename UserState>
+void AStarSearch<UserState>::SetStartAndGoalStates(UserState& start, UserState& goal) {
+
+}
+
+template<typename UserState>
+unsigned int  AStarSearch<UserState>::SearchStep(void) {
+
+}
