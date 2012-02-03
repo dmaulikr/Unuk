72aee6ee1ed428927f968b3e8ec7e283f3762e67
Allanis <allanis@saracraft.net>
2012-02-03 15:15:54 +0000

    [Fix] Changed typename to class in template paremeter list.

--- src/libUnuk/Engine/Pathfinding.cpp
+++ src/libUnuk/Engine/Pathfinding.cpp
@@ -1,23 +1,28 @@
 #include "Pathfinding.h"
 
-template<typename UserState>
+template<class UserState>
 AStarSearch<UserState>::AStarSearch(void) :
 		_state(SEARCH_STATE_NOT_INITIALISED),
 		_currentSolutionNode(NULL),
 		_allocateNodeCount(0),
 		_cancelRequest(false) {}
 
-template<typename UserState>
+template<class UserState>
 AStarSearch<UserState>::~AStarSearch(void) {
 
 }
 
-template<typename UserState>
+template<class UserState>
 void AStarSearch<UserState>::SetStartAndGoalStates(UserState& start, UserState& goal) {
 
 }
 
-template<typename UserState>
+template<class UserState>
 unsigned int  AStarSearch<UserState>::SearchStep(void) {
+
+}
+
+template<class UserState>
+bool AStarSearch<UserState>::AddSuccessor(UserState& state) {
 
 }
