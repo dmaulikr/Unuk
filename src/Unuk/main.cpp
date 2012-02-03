6704954a613391065fbced30c71a64d03adfb9f7
Allanis <allanis@saracraft.net>
2012-02-03 00:16:09 +0000

    [Remove] Removed instantiation of Game* game as KonoM seemd to have moved it.

--- src/Unuk/main.cpp
+++ src/Unuk/main.cpp
@@ -25,20 +25,20 @@
 #endif
 
 static gameNavVal_t RunGame(bool load) {
-  Debug::logger->message("Entering game state..");
-  Game* game = new Game;
-  
-  if(load) {
-    game->Load("save");
-  } else {
-    game->New("save");
-  }
+	Debug::logger->message("Entering game state..");
+	Game* game = new Game;
 
-  gameNavVal_t ret = game->Run();
-  
-  delete game;
-  
-  return ret;
+	if(load) {
+		game->Load("save");
+	} else {
+		game->New("save");
+	}
+
+	gameNavVal_t ret = game->Run();
+
+	delete game;
+
+	return ret;
 }
 
 #if !defined(_WIN32) || defined(_DEBUG)
@@ -47,95 +47,93 @@ int main() {
 int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int) {
 #endif
 
-  Debug::openLog(true);
-  Debug::logger->message("\n----- Engine Loading -----");
+	Debug::openLog(true);
+	Debug::logger->message("\n----- Engine Loading -----");
 
-  if(SDL_Init(SDL_INIT_VIDEO == -1)) {
-    system("zenity --error --text=\"Could not load SDL\"");
-    Debug::logger->message("Error: Could not load SDL");
-    return 1;
-  } else
-    Debug::logger->message("SDL loaded..");
+	if(SDL_Init(SDL_INIT_VIDEO == -1)) {
+		system("zenity --error --text=\"Could not load SDL\"");
+		Debug::logger->message("Error: Could not load SDL");
+		return 1;
+	} else
+		Debug::logger->message("SDL loaded..");
 
-  if(TTF_Init() == -1) {
-    system("zenity --error --text=\"Could not load SDL_TTF\"");
-    Debug::logger->message("Error: Could not load SDL_TTF");
-    return 1;
-  } else
-    Debug::logger->message("SDL_TTF loaded..");
+	if(TTF_Init() == -1) {
+		system("zenity --error --text=\"Could not load SDL_TTF\"");
+		Debug::logger->message("Error: Could not load SDL_TTF");
+		return 1;
+	} else
+		Debug::logger->message("SDL_TTF loaded..");
 
 
 
-  screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32, SDL_HWSURFACE);
-  Debug::logger->message("Video mode set..");
+	screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32, SDL_HWSURFACE);
+	Debug::logger->message("Video mode set..");
 
-  SDL_WM_SetCaption("fps - 00", NULL);
+	SDL_WM_SetCaption("fps - 00", NULL);
 
-  srand((unsigned int)time(NULL));
+	srand((unsigned int)time(NULL));
 
-  camera.x = 0;
-  camera.y = 0;
-  camera.w = SCREEN_WIDTH;
-  camera.h = SCREEN_HEIGHT;
+	camera.x = 0;
+	camera.y = 0;
+	camera.w = SCREEN_WIDTH;
+	camera.h = SCREEN_HEIGHT;
 
-  errorTexture = LoadImage("../Data/Media/error.png");
+	errorTexture = LoadImage("../Data/Media/error.png");
 
-  Text::LoadFonts();
+	Text::LoadFonts();
 
-  Game* game = NULL;
+	Debug::logger->message("Creating mainmenu..");
+	MainMenu* menu = new MainMenu;
 
-  Debug::logger->message("Creating mainmenu..");
-  MainMenu* menu = new MainMenu;
+	Debug::logger->message("\n----- Engine Initialization Complete -----");
+	Debug::logger->message("\n----- Logic -----");
 
-  Debug::logger->message("\n----- Engine Initialization Complete -----");
-  Debug::logger->message("\n----- Logic -----");
+	bool menuRunning = true;
+	while(menuRunning) {
+		switch(menu->Run()) {
+		case mainMenuNewGame:
+			delete menu;
+			switch(RunGame(false)) {
+			case gameMainMenu:
+				menu = new MainMenu;
+				break;
+			case gameQuitGame:
+				menuRunning = false;
+				break;
+			}
+			break;
+		case mainMenuLoadGame:
+			delete menu;
+			switch(RunGame(true)) {
+			case gameMainMenu:
+				menu = new MainMenu;
+				break;
+			case gameQuitGame:
+				menuRunning = false;
+				break;
+			}
+			break;
+		case mainMenuOptions:
+			break;
+		case mainMenuExitGame:
+			menuRunning = false;
+			delete menu;
+			break;
+		}
+	}
+	//stringstream caption;
+	//caption << "Unuk - FPS: " << fps;
 
-  bool menuRunning = true;
-  while(menuRunning) {
-    switch(menu->Run()) {
-    case mainMenuNewGame:
-      delete menu;
-      switch(RunGame(false)) {
-      case gameMainMenu:
-        menu = new MainMenu;
-        break;
-      case gameQuitGame:
-        menuRunning = false;
-        break;
-      }
-      break;
-    case mainMenuLoadGame:
-      delete menu;
-      switch(RunGame(true)) {
-      case gameMainMenu:
-        menu = new MainMenu;
-        break;
-      case gameQuitGame:
-        menuRunning = false;
-        break;
-      }
-      break;
-    case mainMenuOptions:
-      break;
-    case mainMenuExitGame:
-      menuRunning = false;
-      delete menu;
-      break;
-    }
-  }
-  //stringstream caption;
-  //caption << "Unuk - FPS: " << fps;
+	//SDL_WM_SetCaption(caption.str().c_str(), NULL);
 
-  //SDL_WM_SetCaption(caption.str().c_str(), NULL);
+	// Clean up after ourselves.
+	Text::FreeFonts();
 
-  // Clean up after ourselves.
-  Text::FreeFonts();
+	SDL_FreeSurface(screen);
+	SDL_FreeSurface(errorTexture);
 
-  SDL_FreeSurface(screen);
-  SDL_FreeSurface(errorTexture);
+	SDL_Quit();
+	TTF_Quit();
 
-  SDL_Quit();
-  TTF_Quit();
-
-  return 0;
+	return 0;
 }
